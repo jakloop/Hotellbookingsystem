048b87341e7bd67c2fe6dce3ead5635325a0c75a
jakloop <jakobsorl@live.no>
2026-03-06 10:01:30 +0100

    Base class for rooms created solves #1

---
+++ Hotellbookingsystem/Rooms/BaseRoom.cs
@@ -0,0 +1,12 @@
+namespace Hotellbookingsystem.Rooms;
+//<summary>
+// Base class for all rooms
+//</summary>
+public abstract class Room
+{
+    private string roomNumber;
+    private string roomType;
+    private decimal pricePerNight;
+    private bool isAvailable;
+    private int maxGuests;
+}
